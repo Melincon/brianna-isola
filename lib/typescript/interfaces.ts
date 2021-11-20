58aded670658c3e1bea7b504808589a34f4dd87f
Travis Claus <melincon@gmail.com>
2021-11-19 22:56:54 -0500

    Back end api and interface

---
+++ lib/typescript/interfaces.ts
@@ -0,0 +1,15 @@
+export interface PersonInterface {
+    image: string;
+    name: string;
+    caption: string;
+    email: string;
+    header: string;
+    externalLinkLabel: string;
+    externalLink: string;
+    socialLinks?: SocialLinkInterface[];
+}
+
+export interface SocialLinkInterface {
+    icon: string;
+    link: string;
+}
