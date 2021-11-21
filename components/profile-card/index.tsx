2c5596a406daa051fbbf353623520a5b73bbe9eb
Travis Claus <melincon@gmail.com>
2021-11-21 12:54:02 -0500

    Convert back to react app linter and mod css for profile comp

--- components/profile-card/index.tsx
+++ components/profile-card/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import SocialNav from "../social-nav";
 import { PersonInterface } from "../../lib/contentful/interfaces";
+import Image from 'next/image'
 
 export interface ProfileCardProps {
   person: PersonInterface;
@@ -15,10 +16,10 @@ const ProfileCard: FC<ProfileCardProps> = ({ person }) => {
     header,
     externalLink,
   } = person;
-
+  
   return (
     <div className="text-center">
-      <img className="block mx-auto rounded-full w-48 h-48 mb-6" src={profileImage.fields.file.url} alt={profileImage.fields.description} />
+      <img className="block mx-auto w-5/6 h-auto rounded-full mb-6" src={profileImage.fields.file.url} alt={profileImage.fields.description} />
       <h1 className="block text-4xl">{name}</h1>
       <h2 className="block text-xl mb-3">{caption}</h2>
       <div className="border-b w-40 block mx-auto mb-9" />
