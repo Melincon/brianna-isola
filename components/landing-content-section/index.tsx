0efa8181c85f4adce0b12882cb9c282cf4c57140
DESKTOP-J7EOA5P\Brad Bieselin <bradley.bieselin@gmail.com>
2021-11-22 16:34:48 -0500

    added font tracking

--- components/landing-content-section/index.tsx
+++ components/landing-content-section/index.tsx
@@ -9,7 +9,7 @@ const LandingContentSection: FC<LandingContentSectionProps> = ({contentSection})
   const { header, paragraph, headerLink } = contentSection;
   return (
     <div className=" mb-6">
-      <h3 className=" text-3xl text-colorTwo">{header}</h3>
+      <h3 className=" text-3xl text-colorTwo tracking-wide">{header}</h3>
       <div className="border-b mb-3 border-lineColor opacity-30"></div>
       {headerLink && (
         <a className="" href={headerLink.fields.link} target="_blank" rel="noreferrer">{headerLink.fields.linkLabel}</a>
