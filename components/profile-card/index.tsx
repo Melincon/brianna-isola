260b516f889e9c41a9b4b87087d15baa9c129f3c
DESKTOP-J7EOA5P\Brad Bieselin <bradley.bieselin@gmail.com>
2021-11-22 16:26:50 -0500

    Adjusted line opacitiy to 30%

--- components/profile-card/index.tsx
+++ components/profile-card/index.tsx
@@ -24,10 +24,10 @@ const ProfileCard: FC<ProfileCardProps> = ({ person }) => {
       </div>
       <h1 className="block text-4xl text-colorFour">{name}</h1>
       <h2 className="block text-xl mb-3 text-colorTwo font-three italic font-extralight">{caption}</h2>
-      <div className="border-b w-40 block mx-auto mb-9 border-lineColor" />
+      <div className="border-b w-40 block mx-auto mb-9 border-lineColor opacity-30" />
       <span className="block mb-3 text-colorTwo font-three font-extralight">{email}</span>
       <SocialNav />
-      <div className=" border-b w-72 block mx-auto mt-3 border-lineColor" />
+      <div className=" border-b w-72 block mx-auto mt-3 border-lineColor opacity-30" />
       <span className="block text-xl mt-3 text-colorTwo">{header}</span>
       <a className="block text-xl mb-3 text-colorFive" href={externalLink.fields.link} target="_blank" rel="noreferrer" >{externalLink.fields.linkLabel}</a>
     </div>
